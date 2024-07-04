bd9c25d548cf44ecda252253cd4bfbb74316fee8
bdstn10 <budistn108@gmail.com>
2024-07-04 13:45:22 +0700

    mengubah mekanisme pengiriman response file agar sesuai dengan standar di lapangan

--- ytdownloader/downloader/views.py
+++ ytdownloader/downloader/views.py
@@ -1,15 +1,15 @@
 # importing all the required modules 
+from io import BytesIO
 from django.http import FileResponse
 from django.shortcuts import render, redirect 
 import os
 from pytube import *
+import tempfile
 
 # defining function 
 def youtube(request): 
 	# checking whether request.method is post or not 
 	if request.method == 'POST': 
-		path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Downloads')
-  
 		# getting link from frontend 
 		link = request.POST['link'] 
 		video = YouTube(link) 
@@ -17,9 +17,13 @@ def youtube(request):
 		# setting video resolution 
 		stream = video.streams.get_lowest_resolution() 
 		
-		# downloads video 
-		stream.download(output_path=path, skip_existing=False)
-		
-		# returning HTML page 
-		return render(request, 'downloader/youtube.html') 
+		# write media stream to buffer
+		response_file = BytesIO()
+		stream.stream_to_buffer(response_file)
+		response_file.seek(0)
+  
+		# Create FileResponse with the writed buffer
+		response = FileResponse(response_file, as_attachment=True)
+		response['Content-Disposition'] = f'attachment; filename={stream.default_filename}'
+		return response
 	return render(request, 'downloader/youtube.html')
